a884e45a8732508c9210c0be519edf95ce42dd47
Carlos Múgica Gómez <carlos@devanddel.com>
2024-03-20 11:56:06 +0100

    initial commit

--- application/src/main/java/com/helloworld/examples/port/in/users/delete/models/DeleteUserInput.java
+++ application/src/main/java/com/helloworld/examples/port/in/users/delete/models/DeleteUserInput.java
@@ -3,11 +3,12 @@ package com.helloworld.examples.port.in.users.delete.models;
 import com.helloworld.examples.models.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
 @AllArgsConstructor
+@Getter
 public class DeleteUserInput {
-
-	User who;
-	Long userToDelete;
+    User who;
+    Long userToDelete;
 }
