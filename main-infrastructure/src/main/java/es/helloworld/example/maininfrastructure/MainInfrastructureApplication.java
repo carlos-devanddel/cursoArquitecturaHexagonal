01179cb3560b3a8da6e2cb3da7c963a06bebdb46
Carlos Múgica Gómez <carlos@devanddel.com>
2024-03-20 10:54:41 +0100

    initial commit

---
+++ main-infrastructure/src/main/java/es/helloworld/example/maininfrastructure/MainInfrastructureApplication.java
@@ -0,0 +1,13 @@
+package es.helloworld.example.maininfrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainInfrastructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainInfrastructureApplication.class, args);
+    }
+
+}
