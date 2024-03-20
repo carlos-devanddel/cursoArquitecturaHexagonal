f5217dd9a657fb8a4e283d5cbace227c2d9dfedd
Carlos Múgica Gómez <carlos@devanddel.com>
2024-03-20 11:54:29 +0100

    initial commit

--- infrastructure/src/main/java/com/helloworld/examples/frontapi/admin/users/delete/models/DeleteUserResponse.java
+++ infrastructure/src/main/java/com/helloworld/examples/frontapi/admin/users/delete/models/DeleteUserResponse.java
@@ -2,12 +2,16 @@ package com.helloworld.examples.frontapi.admin.users.delete.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.helloworld.examples.port.in.users.delete.models.DeleteUserOutput;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 
-
+@Builder
+@AllArgsConstructor
 public class DeleteUserResponse {
-	@JsonProperty("success")
-	boolean success;
-	public static DeleteUserResponse fromApplicationModel(DeleteUserOutput output) {
-		return new DeleteUserResponse();
-	}
+    @JsonProperty("success")
+    boolean success;
+
+    public static DeleteUserResponse fromApplicationModel(DeleteUserOutput output) {
+        return null;
+    }
 }
