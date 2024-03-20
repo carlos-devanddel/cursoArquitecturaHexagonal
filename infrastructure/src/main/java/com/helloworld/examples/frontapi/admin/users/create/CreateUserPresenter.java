69de0034f98570e65dcecf2d6e9b20f867545485
Carlos Múgica Gómez <carlos@devanddel.com>
2024-03-20 12:08:34 +0100

    initial commit

--- infrastructure/src/main/java/com/helloworld/examples/frontapi/admin/users/create/CreateUserPresenter.java
+++ infrastructure/src/main/java/com/helloworld/examples/frontapi/admin/users/create/CreateUserPresenter.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("frontapi/users/")
+@RequestMapping("frontapi/users")
 @RestController
 public class CreateUserPresenter {
     private final CreateUserUseCase useCase;
