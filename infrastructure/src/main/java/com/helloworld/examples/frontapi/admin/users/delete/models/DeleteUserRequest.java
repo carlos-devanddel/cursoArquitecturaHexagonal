4c3fa8ea6caa870cf08fc3402875a9e0733edd0d
Carlos Múgica Gómez <carlos@devanddel.com>
2024-03-21 11:39:01 +0100

    refactor

--- infrastructure/src/main/java/com/helloworld/examples/frontapi/admin/users/delete/models/DeleteUserRequest.java
+++ infrastructure/src/main/java/com/helloworld/examples/frontapi/admin/users/delete/models/DeleteUserRequest.java
@@ -1,10 +1,18 @@
 package com.helloworld.examples.frontapi.admin.users.delete.models;
 
-import com.helloworld.examples.port.in.users.create.models.CreateUserInput;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.helloworld.examples.port.in.users.delete.models.DeleteUserInput;
 
 public class DeleteUserRequest {
-	public DeleteUserInput toApplicationModel() {
-		return DeleteUserInput.builder().build();
-	}
+    @JsonProperty("who")
+    private String who;
+    @JsonProperty("whoId")
+    private Long whoId;
+
+    @JsonProperty("userToDeleteId")
+    private Long userToDeleteId;
+
+    public DeleteUserInput toApplicationModel() {
+        return DeleteUserInput.builder().build();
+    }
 }
